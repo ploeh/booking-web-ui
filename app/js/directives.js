241aa4815ae6fc142c5ffce290339ccde437df4f
Mark Seemann <mark@ploeh.dk>
2013-09-20 16:13:21 +0200

    Removed the version directive.

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,4 @@
 /* Directives */
 
 
-angular.module('bookingApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('bookingApp.directives', []);
