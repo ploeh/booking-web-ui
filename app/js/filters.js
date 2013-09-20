b4134a8083a52c6409902b41d4c3a36e45afb2ef
Mark Seemann <mark@ploeh.dk>
2013-09-20 16:10:52 +0200

    Removed interpolate filter.

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,4 @@
 
 /* Filters */
 
-angular.module('bookingApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+angular.module('bookingApp.filters', []);
--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -4,16 +4,4 @@
 
 describe('filter', function() {
   beforeEach(module('bookingApp.filters'));
-
-
-  describe('interpolate', function() {
-    beforeEach(module(function($provide) {
-      $provide.value('version', 'TEST_VER');
-    }));
-
-
-    it('should replace VERSION', inject(function(interpolateFilter) {
-      expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
-    }));
-  });
 });
