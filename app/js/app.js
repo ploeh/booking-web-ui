325e4a10db85c057ca9be41ab21992b4136e1217
Mark Seemann <mark@ploeh.dk>
2013-09-20 10:17:48 +0200

    Renamed view1 to home.

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('bookingApp', ['bookingApp.filters', 'bookingApp.services', 'bookingApp.directives', 'bookingApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/home', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/home'});
   }]);
