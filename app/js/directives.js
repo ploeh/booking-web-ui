10d17b0b61f497aaf6cc001f000f6322af4989da
Mark Seemann <mark@ploeh.dk>
2013-09-20 16:03:19 +0200

    Removed the datepicker directive.

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,17 +8,4 @@ angular.module('bookingApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]).
-  directive('datepicker', function($location) {
-  	return {
-  	  link : function(scope, elm, attrs) {
-  	  	elm.datepicker({
-  	  	  dateFormat : 'yy.mm.dd',
-  	  	  onSelect : function(dateText, inst) {
-  	  	  	$location.path('/book/' + dateText);
-  	  	  	scope.$apply();
-  	  	  }
-  	  	});
-  	  } 
-  	}
-  });
+  }]);
--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -5,14 +5,6 @@
 describe('directives', function() {
   beforeEach(module('bookingApp.directives'));
 
-  var datepickerFunction;
-  beforeEach(function() {
-    datepickerFunction = $.fn.datepicker;
-  })
-  afterEach(function() {
-    $.fn.datepicker = datepickerFunction;
-  })
-
   describe('app-version', function() {
     it('should print current version', function() {
       module(function($provide) {
@@ -24,45 +16,4 @@ describe('directives', function() {
       });
     });
   });
-
-  describe('datepicker', function() {
-    it('should invoke the datepicker method', function() {
-      inject(function($compile, $rootScope) {
-        spyOn($.fn, 'datepicker').andCallThrough();
-        var element = $compile('<div datepicker />')($rootScope);
-        expect($.fn.datepicker).toHaveBeenCalled();
-      })
-    });
-
-    it('should invoke datepicker with an onSelect function', function() {
-      inject(function($compile, $rootScope) {
-        spyOn($.fn, 'datepicker').andCallThrough();
-        $compile('<div datepicker />')($rootScope);
-        expect($.fn.datepicker.mostRecentCall.args[0].onSelect).toBeDefined();
-      })
-    });
-
-    it('should have an onSelect function that navigates to correct view', function() {
-      inject(function($compile, $rootScope, $location) {
-        spyOn($.fn, 'datepicker').andCallThrough();
-        spyOn($location, 'path').andCallThrough();
-        spyOn($rootScope, '$apply').andCallThrough();
-        var dateText = '2013.09.28';
-        
-        $compile('<div datepicker />')($rootScope);
-        $.fn.datepicker.mostRecentCall.args[0].onSelect(dateText);
-
-        expect($location.path).toHaveBeenCalledWith('/book/' + dateText);
-        expect($rootScope.$apply).toHaveBeenCalled();
-      })
-    });
-
-    it('should invoke datepicker with the correct date format', function() {
-      inject(function($compile, $rootScope) {
-        spyOn($.fn, 'datepicker').andCallThrough();
-        $compile('<div datepicker />')($rootScope);
-        expect($.fn.datepicker.mostRecentCall.args[0].dateFormat).toEqual('yy.mm.dd');
-      })
-    })
-  })
 });
