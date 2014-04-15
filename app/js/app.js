11bbde4a261739ecf546325d4169ed35bb2b0c2c
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-04-15 15:44:44 +0300

    Injected ng-route module.

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('bookingApp', ['bookingApp.filters', 'bookingApp.services', 'bookingApp.directives', 'bookingApp.controllers']).
+angular.module('bookingApp', ['ngRoute', 'bookingApp.filters', 'bookingApp.services', 'bookingApp.directives', 'bookingApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
     $routeProvider.when('/book/:dateText', {templateUrl: 'partials/book.html', controller: 'BookController'});
